89a3dc50c3766f5e5c40c6adb7e6c4ae1fc77439
raInca <36188750+rafacharlie@users.noreply.github.com>
2019-06-04 21:28:37 +0200

    Corregido #1

--- utiles/Menu.java
+++ utiles/Menu.java
@@ -1,64 +1,63 @@
+/**Clase Menu
+ * 
+ * @author Rafael Infante
+ */
+
 package utiles;
 
-import java.io.IOException;
-
 public class Menu {
-	private String titulo = null;
-	private String opciones[] = null;
-	private int numOpciones = 0;
+  private String titulo = null;
+  private String opciones[] = null;
+  private int numOpciones = 0;
 
-	/**
-	 * 
-	 * @param titulo
-	 *          título del menu
-	 * @param opciones
-	 *          opciones del menu
-	 */
-	public Menu(String titulo, String[] opciones) {
-		this.titulo = titulo;
-		this.opciones = opciones;
-		this.numOpciones = this.opciones.length;
-	}
+  /**
+   * Constructor
+   * 
+   * @param titulo   título del menu
+   * @param opciones opciones del menu
+   */
+  public Menu(String titulo, String[] opciones) {
+    this.titulo = titulo;
+    this.opciones = opciones;
+    this.numOpciones = this.opciones.length;
+  }
 
-	/**
-	 * Gestiona el menu. Consiste en mostrar las opcines y recoger la opcion
-	 * seleccionada por el usuario
-	 * 
-	 * @return opcion valida del menu
-	 * @throws IOException
-	 * @throws NumberFormatException
-	 * @throws NoEsEnteroException
-	 */
-	public int gestionar() throws NumberFormatException, IOException {
-		mostrar();
-		return recogerOpcion();
-	}
+  /**
+   * Gestiona el menu. Consiste en mostrar las opcines y recoger la opcion
+   * seleccionada por el usuario
+   * 
+   * @return opcion valida del menu
+   */
+  public int gestionar() {
+    mostrar();
+    return recogerOpcion();
+  }
 
-	/**
-	 * Muestra las opciones del menu
-	 */
-	private void mostrar() {
-		int i = 1;
-		System.out.println("***" + titulo + "***");
-		for (String elemento : opciones) {
-			System.out.println("(" + (i++) + ") " + elemento);
-		}
-	}
+  /**
+   * Muestra las opciones del menu
+   * 
+   */
+  private void mostrar() {
+    int i = 1;
+    System.out.println("***" + titulo + "***");
+    for (String elemento : opciones) {
+      System.out.println("(" + (i++) + ") " + elemento);
+    }
+  }
 
-	/**
-	 * Recoge la opcion valida del menu
-	 * 
-	 * @return opcion valida
-	 * @throws NoEsEnteroException
-	 */
-	private int recogerOpcion() {
-		int opcion = -1;
-		do {
-			System.out.println("Introduce una opción válida [1, " + numOpciones + "]");
-			
-				opcion = Teclado.leerEntero();
-			
-		} while (opcion < 1 || opcion > numOpciones);
-		return opcion;
-	}
+  /**
+   * Recoge la opcion valida del menu
+   * 
+   * @return opcion valida
+   */
+  private int recogerOpcion() {
+    int opcion = -1;
+    do {
+      System.out.println("Introduce una opción válida [1, " + numOpciones + "]");
+
+      opcion = Teclado.leerEntero();
+
+    } while (opcion < 1 || opcion > numOpciones);
+    return opcion;
+  }
 }
